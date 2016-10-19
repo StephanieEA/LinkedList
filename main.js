9dd9004854b6f1820e4d10cdb6c8ac940dc220b8
Stephanie Andrews <seandrews11@gmail.com>
2016-10-19 13:50:04 -0600

    Add constructor and prototype create

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+function Bookmark (title, url){
+    this.title = title;
+    this.url = url;
 
+Bookmark.prototype.create() = function () {
+  var title = document.querySelector("[name='title']");
+  var url = document.querySelector("[name='url']");
+  var enter = document.querySelector("[name='enter']");
+
+  enter.addEventListener('click', function () {
+    document.createElement("[name='card']");
+    h2.innerText()=title;
+    h3.innerText()=url;
+
+    })
+  }
+}
