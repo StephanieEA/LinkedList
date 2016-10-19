eb71a3570c952eded785262048f102d0d52d4ee8
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-10-19 15:54:08 -0600

    added jQuery append

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
-function Bookmark (title, address) {
-  this.title = title;
-  this.address = address;
-  evenListener for enter button
-  function card () {
-    when enter button is clicked, card is created: title goes to title field, URL goes to URL field and appears on right side of screen.
-  }
-}
+var enter = document.querySelector('.enter');
+var title = document.querySelector('.title');
+var url = document.querySelector('.url');
+
+enter.addEventListener('click', function() {
+  $('.right').append('<h2>The Website Title</h2>'+
+  '<h3>Website URL</h3>'+
+  '<button class="read">Read</button>'+
+  '<button class="delete">Delete</button>');
+})
+
+
+
+
+
+
+
+// function Bookmark (title, url) {
+//   this.title = title;
+//   this.url = url;
+//   // this.card = [];
+// }
+// //sets up construtor function
+//
+//
+//
+// Bookmark.prototype.create() = function() {
+
+//   var card = document.createElement(newCard, [title, url])
+//   card.addEventListener('click', function (){
+//     // var card = document.createElement('.card');
+//     h2.innerText = title;
+//     h3.innerText = url;
+// }
+//
+//
+// //var titleInput = document.querySelector('.title')
+//
+// // var title = "title"
+// // var address = "url"
+// //above is to save input as string to be able to pass it in as argument.
+//
+// // enter.addEventListener('click', function(){
+// //   document.createElement() {
+// //     when enter button is clicked, card is created: title goes to title field, URL goes to URL field and appears on right side of screen.
+// // })
