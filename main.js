df51662cbeb7414e7fbedee6cf71f55239af353a
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-10-19 11:52:31 -0600

    added psuedo code

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function Bookmark (title, address) {
   this.title = title;
   this.address = address;
+  evenListener for enter button
+  function card () {
+    when enter button is clicked, card is created: title goes to title field, URL goes to URL field and appears on right side of screen.
+  }
 }
