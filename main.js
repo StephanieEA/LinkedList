91211bf74c0cbf0b63c7670c77d227d59cb60d16
Stephanie Andrews <seandrews11@gmail.com>
2016-10-21 09:45:07 -0600

    Remove unclear and/or ineffectie JQuery

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-var $enter = $('.enter');
+var enter = document.querySelector('.enter');
 //var readButton = document.querySelector('.read')
 //var newCard = ...
 
-
-$enter.on('click', function(){
-  var title = $title.val();
-  var url = $url.val();
+enter.addEventListener('click', function(){
+  var title = document.querySelector('.title');
+  var url = document.querySelector('.url');
 $('.right').append('<article class="card alreadyread">'+
-'<h2>${Website Title}</h2>'+
-'<h3>${Website URL}</h3>'+
+'<h2>Website Title</h2>'+
+'<h3>Website URL</h3>'+
 '<button class="read red">Read</button>'+
 '<button class="delete">Delete</button>'+
 '</article>');
-// document.querySelector('h2').innerText = title.value;
-// document.querySelector('h3').innerText = url.value;
-
+$('h2').text(title.value);
+$('h3').text(url.value);
+//document.querySelector('h2').innerText = title.value;
+//document.querySelector('h3').innerText = url.value;
 title.value=null;
 url.value=null;
-
 })
-
-readButton.addEventListener('click', function(){
-  readButton.toggleClass(.red)
-  newCard.toggleClass(.alreadyread)
-})
+// readButton.addEventListener('click', function(){
+//   readButton.toggleClass(.red)
+//   newCard.toggleClass(.alreadyread)
+//})
 
 
 //function Bookmark (card) {
