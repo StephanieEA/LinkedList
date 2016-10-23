f99d443c1970d98c9635bc8e7600059306e6f5fb
Stephanie Andrews <seandrews11@gmail.com>
2016-10-23 11:43:21 -0600

    Add parent and child selectors to separate card creation events

--- main.js
+++ main.js
@@ -1,32 +1,49 @@
 var enter = document.querySelector('.enter');
-//var readButton = document.querySelector('.read')
+var readButton = document.querySelector('.read')
 //var newCard = ...
 
 enter.addEventListener('click', function(){
   var title = document.querySelector('.title');
   var url = document.querySelector('.url');
-$('.right').append('<article class="card alreadyread">'+
-'<h2>Website Title</h2>'+
-'<h3>Website URL</h3>'+
-'<button class="read">Read</button>'+
-'<button class="delete">Delete</button>'+
-'</article>');
-$('h2').text(title.value);
-$('h3').text(url.value);
-//document.querySelector('h2').innerText = title.value;
-//document.querySelector('h3').innerText = url.value;
-title.value=null;
-url.value=null;
+  var right = document.querySelector('.parent')
+  var newCard = document.createElement('article');
+
+  newCard.className = 'card';
+  right.appendChild(newCard);
+  $('newCard').append('<article class="card alreadyread">'+
+  '<h2>Website Title</h2>'+
+  '<h3>Website URL</h3>'+
+  '<button class="read">Read</button>'+
+  '<button class="delete">Delete</button>'+
+  '</article>')
+
+  // $('.right').append('<article class="card alreadyread">'+
+  // '<h2>Website Title</h2>'+
+  // '<h3>Website URL</h3>'+
+  // '<button class="read">Read</button>'+
+  // '<button class="delete">Delete</button>'+
+  // '</article>');
+//  $('h2').text(title.value);
+//  $('h3').text(url.value);
+document.querySelector('h2').innerText = title.value;
+document.querySelector('h3').innerText = url.value;
+  title.value=null;
+  url.value=null;
+
+  $('.read').on('click', function () {
+    $('.read').toggleClass('alreadyread');
+    $('.card').toggleClass('darkercard')
+  })
+
+  $('.delete').on('click', function() {
+  $('.card').remove();
+  })
 })
 
-// $('.read').on('click', function () {
-//   $(this).
-//   toggleClass('read')
-// })
 
 
 // readButton.addEventListener('click', function(){
-//   readButton.toggleClass(.red)
+//   .read.toggleClass('read')
 //   newCard.toggleClass(.alreadyread)
 //})
 
