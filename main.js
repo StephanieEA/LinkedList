333db7d3b47903d99ea4844912b83171af02ab65
Stephanie Andrews <seandrews11@gmail.com>
2016-10-23 16:40:56 -0600

    Transfer content from input fields to new cards

--- main.js
+++ main.js
@@ -14,21 +14,16 @@ enter.addEventListener('click', function(){
   '<h3>Website URL</h3>'+
   '<button class="read">Read</button>'+
   '<button class="delete">Delete</button>'+
-  '</article>'
+  '</article>';
+  newCard.querySelector('h2').innerText = title.value;
+  newCard.querySelector('h3').innerText = url.value;
   right.appendChild(newCard);
+  // $('h2').text(title.value);
+  // $('h3').text(url.value);
 
-  // $('.right').append('<article class="card alreadyread">'+
-  // '<h2>Website Title</h2>'+
-  // '<h3>Website URL</h3>'+
-  // '<button class="read">Read</button>'+
-  // '<button class="delete">Delete</button>'+
-  // '</article>');
-// $('h2').text(title.value);
-// $('h3').text(url.value);
-document.querySelector('h2').innerText = title.value;
-document.querySelector('h3').innerText = url.value;
-  title.value=null;
-  url.value=null;
+    title.value=null;
+    url.value=null;
+
 
   $('.read').on('click', function () {
     $('.read').toggleClass('alreadyread');
