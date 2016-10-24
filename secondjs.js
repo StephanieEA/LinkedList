f7b5b7d816fd1f4b7c4571d671be853fe24def07
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-10-23 23:20:41 -0600

    working url tag

--- secondjs.js
+++ secondjs.js
@@ -3,27 +3,101 @@
 var enter = document.querySelector('.enter');
 var readButton = document.querySelector('.read');
 var deleteButton = document.querySelector('.delete');
+var bookmarkSection = document.querySelector('.right');
+var bookmarks = [];
+var id = 0;
 
+// bookmarks --> an array of html fragments, these have an id (which is the index of the array)
+// render() --> will take all html fragemnets and put them on th page
+//
+// listen for clicks on enter
+//   create html fragments with u/t
+//   push that fragment to array
+//
+// render
+//   empty the `.right` section
+//   render fragments on oage
+//
+// read
+//   bubble
+//   change relevenat franemtn
+//     -> render()
+//
+// delet
+//   bubble
+//   change rel fragment
+//     -> render()
 
-enter.addEventListener('click', function() {
-  var title = document.querySelector('.title');
-  var url = document.querySelector('.url');
-  $('.right').append('<article class="card darkercard">'+
-  '<h2>The Website Title</h2>'+
-  '<h3>Website URL</h3>'+
-  '<button class="read alreadyread">Read</button>'+
-  '<button class="delete">Delete</button>'+'</article>');
-  $('h2').text(title.value);
-  $('h3').html(url.value);
-  title.value = null;
-  url.value = null;
-  $('.read').on('click', function() {
-    $('.read').toggleClass('alreadyread');
-    $('.card').toggleClass('darkercard');
+enter.addEventListener('click', function(){
+  var title = document.querySelector('.title').value;
+  var url = document.querySelector('.url').value;
+
+  // make this a global varibale that starts at 0
+  // var id = 0
+  dataId = id++
+
+  var fragment = '<article data-id="' + id + '" class="card darkercard">' +
+                  '<h2>' + title + '</h2>'+
+                  '<h3><a href="' + url + '">' + url + '</a></h3>'+
+                  '<button class="read alreadyread">Read</button>'+
+                  '<button class="delete">Delete</button>'+
+                '</article>';
+
+  bookmarks.push(fragment);
+  render();
+});
+
+function render() {
+  $(bookmarkSection).empty();
+  $(bookmarkSection).append(bookmarks);
+}
+
+bookmarkSection.addEventListener('click', function(event) {
+  var id = $(event.target).closest('.card').data('id');
+
+  var filteredBookmarks = bookmarks.filter(function (fragment) {
+    return $(fragment).data('id') == id;
+  });
+
+  console.log(filteredBookmarks);
+
+  bookmarks = filteredBookmarks;
+  render();
+});
+// enter.addEventListener('click', function() {
+//   var title = document.querySelector('.title').value;
+//   var url = document.querySelector('.url').value;
+//
+//   // adding blob of html to page
+//   $('.right').append('<article class="card darkercard">'+
+//   '<h2>The Website Title</h2>'+
+//   '<h3>Website URL</h3>'+
+//   '<button class="read alreadyread">Read</button>'+
+//   '<button class="delete">Delete</button>'+'</article>');
+//
+//   $('h2').text(title);
+//   // create a func that returns a valid a tag <---- check for https://
+//   $('h3').html("<a href='" + url + "'>" + url + "</a>");
+//
+//   // clearing fields
+//   title.value = null;
+//   url.value = null;
+//
+//
+//   $('.card').on('click', function(e) {
+//     debugger;
+//   })
+
+  // assigning listener to `.read` button
+  // $('.read').on('click', function() {
+  //   $('.read').toggleClass('alreadyread');
+  //   $('.card').toggleClass('darkercard');
     // $('.card').css('background-color', '#F2F4F4');
     // $('.read').css('color', "#F05A28");
-  });
-  $('.delete').on('click', function() {
-    $('.card').remove();
-  })
-})
+  // });
+
+  // assigning listener to .delet button
+  // $('.delete').on('click', function() {
+    // $('.card').remove();
+  // })
+// })
